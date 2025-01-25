b40f025488be8947e357b22337ebc2fffb573265
Linus Theuringer <Linustheuringer@web.de>
2025-01-25 02:48:39 +0100

    ZoomOut
    ZoomOut
    Changed collider to ontrigger

--- Game Jaaaam 2025/Assets/Scripts/S_MapToIsland.cs
+++ Game Jaaaam 2025/Assets/Scripts/S_MapToIsland.cs
@@ -5,7 +5,11 @@ public class S_MapToIsland : ScriptableObject
 {
     [Header("Camera Zoom Settings")]
     [Tooltip("The position the camera will move to when zooming in. This should be the Zoom the player should play on the island.")]
-    public Vector3 targetPosition = new Vector3(0, 0.25f, -0.65f);
+    public Vector3 zoomInPos = new Vector3(0, 0.25f, -0.65f);
+
+    [Tooltip("The position the camera will move to when zooming out. This should be the Zoom the player should play on the main map.")]
+    public Vector3 zoomOutPos = new Vector3(0, 0.5f, -1.5f);
+    
     [Tooltip("The duration of the camera zoom. The lower the value, the faster the camera will zoom in.")]
     public float zoomDuration = 1f; // 1 second
 }
