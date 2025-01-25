5609fb2bc0231f8c0488a0812c7b2ff7340ec7f0
Linus Theuringer <Linustheuringer@web.de>
2025-01-25 21:07:57 +0100

    Why ever can the player not not walk over the water

--- Game Jaaaam 2025/Assets/Scripts/Puddle.cs
+++ Game Jaaaam 2025/Assets/Scripts/Puddle.cs
@@ -1,7 +1,39 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Puddle : MonoBehaviour
 {
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject otherGameObject = other.gameObject;
+        PushableWall pushableWall = otherGameObject.GetComponent<PushableWall>();
+
+        if (other.CompareTag("Player"))
+        {
+            // Prevent player movement
+            StateMachine stateMachine = other.GetComponent<StateMachine>();
+            if (stateMachine != null)
+            {
+                WalkingState walkingState = stateMachine.GetCurrentState() as WalkingState;
+                if (walkingState != null)
+                {
+                    walkingState.DisableMovement();
+                }
+            }
+        }
+        
+        if (IsCompletelyInPuddle(GetComponent<Collider>(), other))
+        {
+            Debug.Log("Puddle OnTriggerEnter");
+            Debug.Log("PushableWall: " + pushableWall);
+
+            if (pushableWall != null)
+            {
+                pushableWall.StartFallingIntoPuddle();
+            }
+        }
+    }
+
     private void OnTriggerStay(Collider other)
     {   
         GameObject otherGameObject = other.gameObject;
@@ -21,16 +53,30 @@ public class Puddle : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        Debug.Log("Puddle OnTriggerExit");
         GameObject otherGameObject = other.gameObject;
         PushableWall pushableWall = otherGameObject.GetComponent<PushableWall>();
         Debug.Log("PushableWall: " + pushableWall);
+
+        if (other.CompareTag("Player"))
+        {
+            // Re-enable player movement
+            StateMachine stateMachine = other.GetComponent<StateMachine>();
+            if (stateMachine != null)
+            {
+                WalkingState walkingState = stateMachine.GetCurrentState() as WalkingState;
+                if (walkingState != null)
+                {
+                    walkingState.EnableMovement();
+                }
+            }
+        }
         
         if (pushableWall != null)
         {
             pushableWall.DestroyWall();
         }
     }
+
     private static bool IsCompletelyInPuddle(Collider puddleCollider, Collider objectCollider)
     {
         // Get bounds of both the puddle and the object
